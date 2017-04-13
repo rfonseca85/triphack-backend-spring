34579b35e566e89729875d039a782b6dfcfa38b3
Rafael Fonseca <rfonseca85@yahoo.ca>
2017-04-13 14:33:28 -0400

    First commit

---
+++ src/main/java/ca/rfonseca85/TriphackBackendSpringApplication.java
@@ -0,0 +1,12 @@
+package ca.rfonseca85;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriphackBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriphackBackendSpringApplication.class, args);
+	}
+}
